9c912d019ad7835cfaac4b2919c770425d735b79
Brian Kao <bk1613@nyu.edu>
2019-09-06 03:22:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(snacks = ["crackers", "gouda", "thyme"])
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find
+  cheese_types.find{}
   if cheese_types.include?(snacks[1])
     puts cheese_types[0]
   else
