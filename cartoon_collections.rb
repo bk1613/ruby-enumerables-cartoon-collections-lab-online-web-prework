ad3a50e55ddc0e1cb70684eb2684bbc34149e655
Brian Kao <bk1613@nyu.edu>
2019-09-06 03:15:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarv_array = ["Doc", "Dopey", "Bashful", "Grumpy"])
   # Your code here
+  dwarv_array.each_with_index do |name, index| 
+    
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
   # Your code here
+  planeteer_calls.map!{|name| name.capitalize }
+  planeteer_calls.map!{|name| name +"!"}
+  planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words = ["puff", "go", "two"])
   # Your code here
+  short_words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks = ["crackers", "gouda", "thyme"])
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if cheese_types.include?(snacks[1])
+    cheese_types[0]
+  else
+    nil
+  end
 end
